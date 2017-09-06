fd53a5ef332aa5355f3717b8242cbc343e7b0705
kajiyama1126 <yuichi11261126@gmail.com>
2017-09-06 17:17:02 +0900

    0906 シミュレーション開始

--- iteration.py
+++ iteration.py
@@ -383,9 +383,9 @@ if __name__ == '__main__':
     lamb = 0.1
     R = 10
     np.random.seed(0)  # ランダム値固定
-    pattern =20
-    test = 10000
-    # step = [0.1, 0.1, 0.25, 0.25, 0.5, 0.5, 1., 1., 2., 2.]
-    step = np.array([[0.1 *(j+1) for i in range(2)] for j in range(10)])
+    pattern = 10
+    test = 1000
+    step = [0.1, 0.1, 0.25, 0.25, 0.5, 0.5, 1., 1., 2., 2.]
+    # step = np.array([[0.1 *(j+1) for i in range(2)] for j in range(10)])
     step = np.reshape(step,-1)
-    tmp = new_iteration_L2(n, m, step, lamb, R, pattern, test)
+    tmp = new_iteration_L1_paper(n, m, step, lamb, R, pattern, test)
